d3a644790acc864491aa2cc4500d61c5d7392018
hyoo14 <hyoo0014@naver.com>
2025-02-15 15:09:20 -0500

    Update backtranslation_attack.py

--- attacks/backtranslation_attack.py
+++ attacks/backtranslation_attack.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-# DNA codon to amino acid translation table
+
 dna_to_aa_table = {
     'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
     'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
@@ -24,7 +24,7 @@ dna_to_aa_table = {
     'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
 }
 
-# Reverse translation table from amino acids to DNA codons
+
 aa_to_dna_table = {v: [k for k in dna_to_aa_table if dna_to_aa_table[k] == v] for v in set(dna_to_aa_table.values())}
 
 def dna_to_aa(sequence):
@@ -35,9 +35,9 @@ def aa_to_dna(aa_sequence):
 
 def back_translation(sequence):
     aa_sequence = dna_to_aa(sequence)
-    #print(aa_sequence)
+    
     translated_dna = aa_to_dna(aa_sequence)
-    #print(translated_dna)
+    
     return translated_dna
 
 def backtranslation_attack(sequences, mutation_rate=None, iteration=None):
@@ -50,12 +50,4 @@ def backtranslation_attack(sequences, mutation_rate=None, iteration=None):
 
 
 
-# test_dir = "/content/drive/MyDrive/RDL/prj/data/test.csv"
-# test_df = pd.read_csv(f"{test_dir}")
-# test_df['sequence'] = test_df['sequence'].str.upper()
-
-# iternum = 1
-# mutation_rate=0.1
-
-# test_df['sequence'] = backtranslation_attack(test_df['sequence'], mutation_rate, iternum)
 
