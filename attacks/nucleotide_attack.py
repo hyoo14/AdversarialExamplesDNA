23746990944134550fc1fd848800794dec2b0382
hyoo14 <hyoo0014@naver.com>
2025-02-15 15:09:51 -0500

    Update nucleotide_attack.py

--- attacks/nucleotide_attack.py
+++ attacks/nucleotide_attack.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 
 
-# Define mutation functions
+
 def nucleotide_mutation(sequence, mutation_rate=0.1):
     sequence = list(sequence)
     for i in range(len(sequence)):
@@ -20,13 +20,3 @@ def nucleotide_attack(sequences, mutation_rate=0.1, iteration=1):
   return mutated_sequences
 
 
-
-# test_dir = "/content/drive/MyDrive/RDL/prj/data/test.csv"
-# test_df = pd.read_csv(f"{test_dir}")
-# test_df['sequence'] = test_df['sequence'].str.upper()
-
-# iternum = 1
-# mutation_rate=0.1
-
-# test_df['sequence'] = nucleotide_attack(test_df['sequence'], mutation_rate, iternum)
-# test_df.to_csv("/content/drive/MyDrive/RDL/prj/data/test_atk_nucl_iter1_rate0_1.csv")
